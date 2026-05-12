547b8c31ad1669b271a3ffed7104dac66961c16f
Senal-D-A-Gunaratna <230862861+Senal-D-A-Gunaratna@users.noreply.github.com>
2026-05-12 19:13:50 +0530

    matugen loader edit

--- plugins/matugen-loader.lua
+++ plugins/matugen-loader.lua
@@ -1,18 +1,13 @@
 -- load the matugen theme
 
-local enabled = true -- Set to false to go back to "Normal" mode
+local enabled = true -- Set to false to disable matugen theme
 
 if not enabled then
+  vim.notify("matugen: theme disabled", vim.log.levels.WARN)
   return {}
 end
 
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = function() end, -- noop, matugen handles it
-    },
-  },
   {
     dir = vim.fn.stdpath("config"),
     name = "matugen-theme",
